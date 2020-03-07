021cb56dbc9501269ea7ae3c839791af5bd9d44d
mahaboobpasha115 <mahaboobpashapasha115@gmail.com>
2020-03-07 21:33:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+//using for loop for printing array elements
+
+
+var array=["sam", "tom", "curran", "wilson","mortal"];
+function names(){
+    for(n=0;n<array.length;n++)
+        console.log(array[n]);
+        
+    }
+    names()
+
+
+    var array=[1,5,3,7,2,9,6];
+    function numbers(){
+        for(m=0;m<array.length;m++)
+        console.log(array[m]);
+        
+    }
+    numbers()
+
+ //array properties
+
+    var array=[5,6,2,1,9,8]
+    array.push(3,4);
+    console.log(array);
+    array.pop();
+    console.log(array);
+    array.shift();
+    console.log(array);
+    array.unshift(5);
+    console.log(array);
+
+    
+    
+    
+    
